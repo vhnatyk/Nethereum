105e72b29859c39a588103016a7246dd269ef001
vhnatyk <vhnatyk@gmail.com>
2018-06-10 08:54:24 +0300

    to local

--- testchain/clique/startgeth.bat
+++ testchain/clique/startgeth.bat
@@ -1,3 +1,15 @@
+<<<<<<< Updated upstream
 RD /S /Q %~dp0\devChain\geth
 geth  --datadir=devChain init genesis_clique.json
 geth --nodiscover --rpc --datadir=devChain  --rpccorsdomain "*" --mine --rpcapi "eth,web3,personal,net,miner,admin,debug" --rpcaddr "0.0.0.0" --unlock 0x12890d2cce102216644c59daE5baed380d84830c --password "pass.txt" --verbosity 0 console  
+=======
+RD /S /Q %~dp0\devChain\geth\chainData
+RD /S /Q %~dp0\devChain\geth\dapp
+RD /S /Q %~dp0\devChain\geth\nodes
+del %~dp0\devchain\geth\nodekey
+
+cd C:\Projects\Repos\EthereumC\Nethereum\Nethereum\testchain
+REM geth.exe --rinkeby --syncmode "fast" --datadir=devchain --rpc --rpccorsdomain "*" --rpcapi "eth,web3,personal,net,miner,admin,debug" --verbosity 0 console
+geth.exe --testnet --syncmode "fast" --datadir=devchain --rpc --rpccorsdomain "*" --rpcapi "eth,web3,personal,net,miner,admin,debug" --verbosity 0 console
+eth.getBalance(eth.coinbase)
+>>>>>>> Stashed changes
--- testchain/startgeth.bat
+++ testchain/startgeth.bat
@@ -3,5 +3,9 @@ RD /S /Q %~dp0\devChain\geth\dapp
 RD /S /Q %~dp0\devChain\geth\nodes
 del %~dp0\devchain\geth\nodekey
 
-geth.exe  --datadir=devChain init genesis_dev.json
-geth.exe --mine --rpc --networkid=39318 --cache=2048 --maxpeers=0 --datadir=devChain  --ipcpath "geth.ipc"  --rpccorsdomain "*" --rpcapi "eth,web3,personal,net,miner,admin,debug" --verbosity 0 console  
+cd C:\Projects\Repos\EthereumC\Nethereum\Nethereum\testchain
+REM geth.exe --rinkeby --syncmode "fast" --datadir=devchain --rpc --rpccorsdomain "*" --rpcapi "eth,web3,personal,net,miner,admin,debug" --verbosity 0 console
+rem geth --testnet removedb
+REM geth.exe --testnet --syncmode "fast" --datadir=devchain --rpc --rpccorsdomain "*" --rpcapi "eth,web3,personal,net,miner,admin,debug" --verbosity 0 console
+REM web3.eth.getBalance('0xee865de6ebbbe517d079cf5effb274670284963e')
+geth --testnet --syncmode "fast" --datadir=ropstenChain --rpc --rpcapi db,eth,net,web3,personal --cache=1024  --rpcport 8545 --rpcaddr 127.0.0.1 --rpccorsdomain "*" --bootnodes "enode://20c9ad97c081d63397d7b685a412227a40e23c8bdc6688c6f37e97cfbc22d2b4d1db1510d8f61e6a8866ad7f0e17c02b14182d37ea7c3c8b9c2683aeb6b733a1@52.169.14.227:30303,enode://6ce05930c72abc632c58e2e4324f7c7ea478cec0ed4fa2528982cf34483094e9cbc9216e7aa349691242576d552a2a56aaeae426c5303ded677ce455ba1acd9d@13.84.180.240:30303" console
